348f8e49f3587ca0c11cb0d00153cc276263f116
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-04-09 20:17:26 -0500

    Chore: Inicializar projeto

---
+++ src/EndpointValidator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EndpointValidator {
+    public static String validateEndpoint(String endpoint) {
+        // TODO: Verifique se o endpoint fornecido segue o formato padrão de endpoint de API:
+        
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String endpoint = scanner.nextLine();
+        System.out.println(validateEndpoint(endpoint));
+    }
+}
