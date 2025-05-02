bf71d1bbd93c9c360ac4591e3a657f78d35e7ab1
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-05-02 11:50:49 -0500

    Feat: Implementar solução do desafio 7
    - Adiciona novo funcionário à lista com ID sequencial, nome e cargo lidos.

--- src/dev/hermann/desafio7/GerenciamentoFuncionarios.java
+++ src/dev/hermann/desafio7/GerenciamentoFuncionarios.java
@@ -23,22 +23,19 @@ public class GerenciamentoFuncionarios {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Funcionario> funcionarios = new ArrayList<>();
-        
-        
+
         int n = scanner.nextInt();
         scanner.nextLine();
-        
+
         for (int i = 0; i < n; i++) {
-         
+
             String nome = scanner.nextLine();
             String cargo = scanner.nextLine();
-            
-          // TODO: Adicione funcionários à lista 'funcionarios':
-         
-  
+
+            // Adiciona funcionários à lista 'funcionarios':
+            funcionarios.add(new Funcionario(i + 1, nome, cargo));
         }
-        
-  
+
         Collections.sort(funcionarios, Comparator.comparingInt(f -> f.id));
         for (Funcionario f : funcionarios) {
             System.out.println(f);
